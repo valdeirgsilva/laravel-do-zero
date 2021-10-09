f31f8bbaa3b509a7fe3e6d72574b18d25691d99e
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2021-10-09 13:11:20 -0300

    Formulário de contato parte 1.

--- resources/views/site/contact/index.blade.php
+++ resources/views/site/contact/index.blade.php
@@ -7,15 +7,16 @@
             <p>Hercle, abnoba dexter!, racana! Hippotoxota noceres, tanquam gratis luna. Lotus, noster lactas rare captis de fortis, audax omnia.</p>
             <section class="contact__options">
 
-                <form class="contact__form">
-                    <label for="text">Nome completo</label>
-                    <input id="text" name="FullName" type="text" required tabindex="1" placeholder="Ex: José da Silva" autofocus>
+                <form class="contact__form" action="{{route('site.contact.form')}}" method="post">
+                    @csrf
+                    <label for="name">Nome completo</label>
+                    <input id="name" name="name" type="text" required tabindex="1" placeholder="Ex: José da Silva" autofocus>
 
-                    <label for="text">Email</label>
-                    <input id="email" name="mail" type="email" required tabindex="2" placeholder="Ex: email@email.com.br">
+                    <label for="email">Email</label>
+                    <input id="email" name="email" type="email" required tabindex="2" placeholder="Ex: email@email.com.br">
 
                     <label for="message">Mensagem</label>
-                    <textarea id="message" name="WrirteMessage"  required tabindex="3"  cols="20" rows="4" placeholder="Digite aqui..."></textarea>
+                    <textarea id="message" name="message"  required tabindex="3"  cols="20" rows="4" placeholder="Digite aqui..."></textarea>
 
                     <button class="button button_primary" type="submit">Enviar mensagem</button>
                 </form>
